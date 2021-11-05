2920450d5503b7db84aa44e2db0ae0398af3cd26
cjfelixx <felixclyde@gmail.com>
2021-11-04 16:47:28 -1000

    Fied data visualizations

--- FederatedLearing/fl_async.py
+++ FederatedLearing/fl_async.py
@@ -1,19 +1,20 @@
 import numpy as np
 
-def fl_sync(iteration,K,kernel,h,u,d,l):
+def fl_async(iteration,K,kernel,h,u,d,p):
     D = len(h)
     c =  np.ones(K).reshape(K,1)/K    
     mse = np.zeros(iteration)
     alpha = np.zeros((D,1))  
     alphas = np.zeros((K,D))
-    
+    last_alphas = alphas.copy()
     mse[0] = np.var(d)
-
+    edge_count = [ 0 for i in range(K)]
     for n in range(iteration):
 
         # Local updates
         v = np.random.randint(len(d))
         edge = np.random.randint(K)
+        edge_count[edge] += 1
         alpha_in = alphas[edge].reshape((D,1))
         u_k = u[v]
         h_k = h[:,v].reshape((D,1))
@@ -22,11 +23,13 @@ def fl_sync(iteration,K,kernel,h,u,d,l):
 
         alphas[edge] = alpha_step.T
 
-        if n % l == 0 and n>0:
-            alpha = (alphas.T @ c)
-            alphas = np.repeat(alpha,K,axis=1).T
+        if edge_count[edge] % p == 0 and n>0:
+            alpha += (alphas[edge] - last_alphas[edge]).reshape((D,1))/p
+            alphas[edge] = alpha.reshape((D,))
+            last_alphas[edge] = alphas[edge]
             mse[n] = np.square(np.linalg.norm(d[-500::].reshape(500,1) - h.T[-500::] @ alpha))/500
+            edge_count[edge] = 0
         elif n > 0:
-
-            mse[n] = mse[n-1]        
+            mse[n] = mse[n-1]    
+        
     return mse
